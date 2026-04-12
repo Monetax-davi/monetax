b8d11ac338d50df4a6c3521db94ac4394f6f6363
Monetax-davi <daviezequielberaldo72@gmail.com>
2026-04-12 04:32:33 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   async redirects() {
     return [
-      { source: '/', destination: '/monetax-LP-v4-corrigida__2_.html', permanent: false },
+      { source: '/', destination: '/lp.html', permanent: false },
       { source: '/contato', destination: '/contato.html', permanent: false },
       { source: '/politica-de-privacidade', destination: '/politica-de-privacidade.html', permanent: false },
       { source: '/termos-de-uso', destination: '/termos-de-uso.html', permanent: false },
